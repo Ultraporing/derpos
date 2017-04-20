175f9c1e6bd781969c36f86eb7cd36b79fe148cb
Tobias Lenz <Black_D@gmx.de>
2017-04-20 16:30:54 +0200

    more serial stuff

--- ioports.h
+++ ioports.h
@@ -34,4 +34,23 @@
 */
 void serial_configure_baud_rate(unsigned short com, unsigned short divisor);
 
+/** serial_configure_line:
+*  Configures the line of the given serial port. The port is set to have a
+*  data length of 8 bits, no parity bits, one stop bit and break control
+*  disabled.
+*
+*  @param com  The serial port to configure
+*/
+void serial_configure_line(unsigned short com);
+
+/** serial_is_transmit_fifo_empty:
+*  Checks whether the transmit FIFO queue is empty or not for the given COM
+*  port.
+*
+*  @param  com The COM port
+*  @return 0 if the transmit FIFO queue is not empty
+*          1 if the transmit FIFO queue is empty
+*/
+int serial_is_transmit_fifo_empty(unsigned int com);
+
 #endif /* INCLUDE_IO_H */
