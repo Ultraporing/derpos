d6ac0c85d43f96fd2626fe2bfcd62cf7d1a59155
Tobias Lenz <black_d@gmx.de>
2017-05-11 21:26:49 +0000

    auto dependency installer script

---
+++ instDepends.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# instDepends.sh
+ 
+echo "install Build Dependencies..."
+sudo apt-get update
+sudo apt-get -y install build-essential
+sudo apt-get -y install nasm
+sudo apt-get -y install genisoimage
+sudo apt-get -y install bochs
+sudo apt-get -y install bochs-sdl
+
+echo "install package for GNU global..."
+sudo apt-get -y install curl
+sudo apt-get -y install wget
+sudo apt-get -y install ncurses-dev
+sudo apt-get -y install exuberant-ctags
+ 
+sudo apt-get -y install python
+ 
+CUR=`pwd`
+echo "install GNU global..."
+wget http://tamacom.com/global/global-6.4.tar.gz
+tar zxvf global-6.4.tar.gz
+cd global-6.4
+./configure
+make
+sudo make install
+cd $CUR
+ 
+echo "install pip..."
+sudo apt-get -y install python-pip
+ 
+echo "install pygments..."
+sudo pip install pygments==1.6
+ 
+echo "$0 done."
+exit 0
